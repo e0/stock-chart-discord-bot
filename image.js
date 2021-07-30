6e11827a51a926f16ca162f426a2145d741254f5
e0 <shan.huang@me.com>
2021-07-30 14:50:44 +0200

    fix: do not run browser in sandbox mode

--- image.js
+++ image.js
@@ -5,7 +5,9 @@ let browser
 
 const generateImage = async (symbol) => {
   if (!browser) {
-    browser = await puppeteer.launch()
+    browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    })
   }
 
   const page = await browser.newPage()
