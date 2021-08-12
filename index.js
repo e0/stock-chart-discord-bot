d852e0fb40930f59cca26c67ac603616e3ec871e
e0 <shan.huang@me.com>
2021-08-12 13:20:33 +0200

    feat: add em dash support for date modifier

--- index.js
+++ index.js
@@ -13,15 +13,27 @@ client.on('message', async (msg) => {
     const [command, symbol, ...args] = msg.content.split(' ')
     let timeframe, dateString
 
-    if (args.includes('-w') || args.includes('--weekly') || args.includes('—weekly')) {
+    if (
+      args.includes('-w') ||
+      args.includes('--weekly') ||
+      args.includes('—weekly')
+    ) {
       timeframe = 'weekly'
     }
 
-    if (args.includes('-m') || args.includes('--monthly') || args.includes('—monthly')) {
+    if (
+      args.includes('-m') ||
+      args.includes('--monthly') ||
+      args.includes('—monthly')
+    ) {
       timeframe = 'monthly'
     }
 
-    if (args.includes('-h') || args.includes('--hourly') || args.includes('—hourly')) {
+    if (
+      args.includes('-h') ||
+      args.includes('--hourly') ||
+      args.includes('—hourly')
+    ) {
       timeframe = 'hourly'
     }
 
@@ -29,6 +41,8 @@ client.on('message', async (msg) => {
       dateString = args[args.indexOf('-d') + 1]
     } else if (args.includes('--date')) {
       dateString = args[args.indexOf('--date') + 1]
+    } else if (args.includes('—date')) {
+      dateString = args[args.indexOf('—date') + 1]
     }
 
     try {
