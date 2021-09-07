6a341050fa2b4051f47dcfc076aedb017ccc6be4
e0 <shan.huang@me.com>
2021-09-07 23:03:09 +0200

    feat: add support for multiple chart commands in multi-line messages
    This resolves #11

--- index.js
+++ index.js
@@ -20,55 +20,63 @@ client.on('messageCreate', async (msg) => {
     const [command, symbol, ...args] = msg.content.split(' ')
     let timeframe, dateString
 
-    if (
-      args.includes('-w') ||
-      args.includes('--weekly') ||
-      args.includes('—weekly')
-    ) {
-      timeframe = 'weekly'
-    }
+  const lines = msg.content.split('\n')
 
-    if (
-      args.includes('-m') ||
-      args.includes('--monthly') ||
-      args.includes('—monthly')
-    ) {
-      timeframe = 'monthly'
-    }
+  for (const line of lines) {
+    if (line.startsWith('!chart ')) {
+      const [command, symbol, ...args] = line.split(' ')
+      let timeframe, dateString
 
-    if (
-      args.includes('-h') ||
-      args.includes('--hourly') ||
-      args.includes('—hourly')
-    ) {
-      timeframe = 'hourly'
-    }
-
-    if (args.includes('-d')) {
-      dateString = args[args.indexOf('-d') + 1]
-    } else if (args.includes('--date')) {
-      dateString = args[args.indexOf('--date') + 1]
-    } else if (args.includes('—date')) {
-      dateString = args[args.indexOf('—date') + 1]
-    }
-
-    try {
-      let imageStream
-
-      try {
-        imageStream = await getCachedImage(symbol, timeframe, dateString)
-      } catch {
-        imageStream = await generateImage(symbol, timeframe, dateString)
+      if (
+        args.includes('-w') ||
+        args.includes('--weekly') ||
+        args.includes('—weekly')
+      ) {
+        timeframe = 'weekly'
       }
 
-      const attachment = new MessageAttachment(imageStream)
-      msg.channel.send({ files: [attachment] })
-    } catch (e) {
-      msg.channel.send(
-        `Chart could not be loaded for ${symbol}, please try later.`,
-      )
+      if (
+        args.includes('-m') ||
+        args.includes('--monthly') ||
+        args.includes('—monthly')
+      ) {
+        timeframe = 'monthly'
+      }
 
-      console.error(e)
+      if (
+        args.includes('-h') ||
+        args.includes('--hourly') ||
+        args.includes('—hourly')
+      ) {
+        timeframe = 'hourly'
+      }
+
+      if (args.includes('-d')) {
+        dateString = args[args.indexOf('-d') + 1]
+      } else if (args.includes('--date')) {
+        dateString = args[args.indexOf('--date') + 1]
+      } else if (args.includes('—date')) {
+        dateString = args[args.indexOf('—date') + 1]
+      }
+
+      try {
+        let imageStream
+
+        try {
+          imageStream = await getCachedImage(symbol, timeframe, dateString)
+        } catch {
+          imageStream = await generateImage(symbol, timeframe, dateString)
+        }
+
+        const attachment = new MessageAttachment(imageStream)
+        msg.channel.send({ files: [attachment] })
+      } catch (e) {
+        msg.channel.send(
+          `Chart could not be loaded for ${symbol}, please try later.`,
+        )
+
+        console.error(e)
+      }
     }
   }
 })
