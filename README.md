69f3b6ba22583858ee56134ecb70838984d796b7
e0 <shan.huang@me.com>
2023-01-04 01:13:41 +0100

    feat: add earnings bot

--- README.md
+++ README.md
@@ -5,22 +5,25 @@ Discord bot for posting stock charts. Based on [stock-chart-generator](https://g
 ## Prerequisites
 
 - [Node.js](https://nodejs.org/en/)
-- [yarn](https://yarnpkg.com)
+- [pnpm](https://pnpm.io)
 
 ## Get started
 
-1. Install dependencies: `yarn`
+1. Install dependencies: `pnpm install`
 2. Add a `.env` file in the project root with the following lines, fill in your own values.
 
 ```
 DISCORD_API_TOKEN=
 STOCK_CHART_GENERATOR_URL=
 STOCK_CHART_WORKER_URL=
+FMP_API_URL=
+FMP_API_KEY=
 ```
 
-3. Run in dev: `yarn dev`
+3. Run in dev: `pnpm dev`
 4. For deployment, TBD.
 
 ## Commands
 
 - `!chart SYMBOL`: generates a daily chart for the provided symbol
+- `!setup-daily-earnings`: sets up the bot to post daily earnings tickers
---
+++ earnings.js
@@ -0,0 +1,68 @@
+const fetch = require('node-fetch')
+const fs = require('fs')
+
+const formatEarnings = (date, data) => {
+  const earnings = data.reduce(
+    (acc, curr) => {
+      const { symbol, time } = curr
+      acc[time].push(symbol)
+      return acc
+    },
+    { bmo: [], amc: [] },
+  )
+
+  const text = `**Earnings ${date} from FMP**
+
+***Before Market Open*** (${earnings.bmo.length})
+${earnings.bmo.sort().join(', ')}
+
+***After Market Close*** (${earnings.amc.length})
+${earnings.amc.sort().join(', ')}`
+
+  return text
+}
+
+const registerChannel = (guildId, channelId) => {
+  if (!fs.existsSync('./earningsChannels.json')) {
+    fs.writeFileSync('./earningsChannels.json', JSON.stringify([]))
+  }
+  const earningsChannels = require('./earningsChannels.json')
+
+  const existingChannel = earningsChannels.find(
+    (channel) => channel.guildId === guildId && channel.channelId === channelId,
+  )
+
+  if (!existingChannel) {
+    earningsChannels.push({
+      guildId,
+      channelId,
+    })
+    fs.writeFileSync(
+      './earningsChannels.json',
+      JSON.stringify(earningsChannels, null, 2),
+    )
+  }
+}
+
+const getDailyEarnings = async (guildId, channelId) => {
+  if (guildId && channelId) {
+    registerChannel(guildId, channelId)
+  }
+
+  const date = new Date().toLocaleDateString('en-CA', {
+    timeZone: 'America/New_York',
+  })
+
+  const url = `${process.env.FMP_API_URL}/v3/earning_calendar?from=${date}&to=${date}&apikey=${process.env.FMP_API_KEY}`
+
+  const results = await fetch(url)
+
+  if (results.ok) {
+    const data = await results.json()
+    return data.length ? formatEarnings(date, data) : null
+  }
+
+  return null
+}
+
+module.exports = { getDailyEarnings }
